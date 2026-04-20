8b03643ee5ba3e3f6d987f35570269aec4a573bb
Sakethram Katakam <80837734+Sakethram1004@users.noreply.github.com>
2026-04-20 18:28:15 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import CollegeERP from "./CollegeERP";
+
+function App() {
+  return <CollegeERP />;
+}
+
+export default App;
